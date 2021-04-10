0837d93e8b740ad797e1b986a82491b60daa78bd
Santiago Urrego <stg.72@hotmail.com>
2021-04-09 20:15:43 -0500

    se modifica administracion de productos

--- Client/webapp/app/controllers/adminProducts.js
+++ Client/webapp/app/controllers/adminProducts.js
@@ -8,11 +8,19 @@ function cvAdminProducts(masterData, global) {
     /*Miembros del controlador*/
     var vmAdminProducts = this;
     vmAdminProducts.goToPage = goToPage;
-    vmAdminProducts.adminProducts = [];
+    vmAdminProducts.listProducts = [];
+    vmAdminProducts.dataProducts = [];
+    vmAdminProducts.getAdminProductOne = getAdminProductOne;
+    vmAdminProducts.saveData = saveData;
+    vmAdminProducts.clearForm = clearForm;
+    vmAdminProducts.Formulario = false;
+    vmAdminProducts.btnAction = 'Guardar';
+
     
     vmAdminProducts.init = function() {
         //Función inicial
         getAdminProducts();
+        configDataTable();
     };
 
     function goToPage(page) {
@@ -21,12 +29,78 @@ function cvAdminProducts(masterData, global) {
 
     // Función de ejemplo para obtener datos
     function getAdminProducts() {
+        vmAdminProducts.listProducts = []
         masterData.getData('api/producto')
             .then(function(data) {
                 data.data.forEach(element => {
-                    vmAdminProducts.adminProducts.push(element)
+                    vmAdminProducts.listProducts.push(element)
                 });
             });
     }
 
+    //Función para obtener un producto
+    function getAdminProductOne(id) {
+        vmAdminProducts.Formulario = true; //Manejo de formulario
+        masterData.getDataById('api/producto/', id)
+            .then(function(data) {
+                vmAdminProducts.dataProducts = data.data
+                vmAdminProducts.btnAction = 'Actualizar'
+            });
+    }
+
+    //Función para guardar datos
+    function saveData(dataProduct) {
+        if(Object.keys(dataProduct).length > 0) {
+            if(dataProduct.id) {
+                updateData(dataProduct)
+            } else {
+                var object = {
+                "Nombre": dataProduct.Nombre,
+                "Cantidad": dataProduct.Cantidad,
+                "ValorUnitario": dataProduct.ValorUnitario,
+                "FechaVencimiento": dataProduct.FechaVencimiento,
+                "Estado": dataProduct.Estado,
+                // "NombreImagen": dataProduct.NombreImagen
+                }
+                masterData.send('api/producto', object)
+                .then(function(data) {
+                    if (data.data.message) {
+                        swal("Exito", data.data.message, "success");
+                        getAdminProducts();
+                    } else {
+                        swal('Error');
+                    }
+                });
+            }
+        } else {
+            //  validation
+            swal("Error", 'Debe llenar el formulario', "error");
+        }
+    }
+    
+    //Función para actualizar datos
+    function updateData(data) {
+        masterData.UpdateData('api/producto/' + data.id, data)
+        .then(function(data) {
+                if (data.data.message) {
+                    swal("Exito", data.data.message, "success");
+                    getAdminProducts();
+                } else {
+                    swal('Error');
+                }
+
+            });
+
+    }
+
+    function clearForm() {
+        vmAdminProducts.dataProducts = []
+        vmAdminProducts.btnAction = 'Guardar'
+    }
+
+    function configDataTable() {
+        $(document).ready(function() {
+            $('#tableProduct').DataTable();
+        } );
+    }
 }
--- Server/src/controllers/ProductosController.ts
+++ Server/src/controllers/ProductosController.ts
@@ -12,30 +12,30 @@ class GamesController {
 
     public async getOne(req: Request, res: Response): Promise<any> {
         const { id } = req.params;
-        const games = await pool.query('SELECT * FROM producto WHERE id = ?', [id]);
-        console.log(games.length);
-        if (games.length > 0) {
-            return res.json(games[0]);
+        const data = await pool.query('SELECT * FROM producto WHERE id = ?', [id]);
+        if (data.length > 0) {
+            return res.json(data[0]);
         }
-        res.status(404).json({ text: "The game doesn't exits" });
+        res.status(404).json({ text: "El producto consultado no existe." });
     }
 
     public async create(req: Request, res: Response): Promise<void> {
+        // console.log(req.body)
         const result = await pool.query('INSERT INTO producto set ?', [req.body]);
-        res.json({ message: 'Game Saved' });
+        res.json({ message: 'Se ha creado el producto exitosamente.' });
     }
 
     public async update(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
-        const oldGame = req.body;
+        // const oldGame = req.body;
         await pool.query('UPDATE producto set ? WHERE id = ?', [req.body, id]);
-        res.json({ message: "The game was Updated" });
+        res.json({ message: "Se ha actualizado el producto exitosamente." });
     }
 
     public async delete(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
         await pool.query('DELETE FROM producto WHERE id = ?', [id]);
-        res.json({ message: "The game was deleted" });
+        res.json({ message: "Se ha eliminado el producto exitosamente." });
     }
 }
 
